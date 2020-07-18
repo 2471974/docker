4b4bd100c835c4846719b68356be068e112b0a92
2471974 <24.7.1974@gmail.com>
2020-07-18 01:24:56 -0600

    Update

---
+++ src/main/java/guru/spring/converters/ProductToProductForm.java
@@ -0,0 +1,22 @@
+package guru.spring.converters;
+
+import guru.spring.commands.ProductForm;
+import guru.spring.models.Product;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+@Component
+public class ProductToProductForm implements Converter<Product, ProductForm> {
+    @Override
+    public ProductForm convert(Product product) {
+        ProductForm productForm = new ProductForm();
+        productForm.setId(product.getId().toHexString());
+        productForm.setDescription(product.getDescription());
+        productForm.setPrice(product.getPrice());
+        productForm.setImageUrl(product.getImageUrl());
+        return productForm;
+    }
+}
